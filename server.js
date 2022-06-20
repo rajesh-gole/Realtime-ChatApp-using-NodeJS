3f8004662823ce77652dfbc8b7b6636b9e16ff5d
RAJESH GOLE <rajeshgole96@gmail.com>
2022-06-20 12:13:56 +0530

    create server and design frontend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+
+
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT, () => {
+    console.log(`Listing to PORT ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html')
+})
